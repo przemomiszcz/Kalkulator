dfc1e36fd2c3b90f49b742dd1c741c9eb14b0ac6
przemomiszcz <przemek.rzedkowski@interia.eu>
2016-01-03 21:17:19 +0100

    Poczatek formularza

---
+++ src/Form.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Dante on 03.01.2016.
+ */
+public class Form {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 /**
  * Created by Dante on 01.01.2016.
  */
+
 public class Main {
+
+    public static void main(String []args){
+        System.out.println("ass");
+    }
 }
